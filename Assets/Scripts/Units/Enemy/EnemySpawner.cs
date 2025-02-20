a511c329cc8b76eb0587571b496dadf73248eeda
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-02-20 19:41:47 +0900

    [Add]Random place spawn
    need spawning logics by the time flows.

--- Assets/Scripts/Units/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Units/Enemy/EnemySpawner.cs
@@ -5,13 +5,48 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
+    private Transform placeToSpawnEnemy;
+
+    [SerializeField]
     private GameObject[] enemy;
+
+    private int numberToSpawn;
+
+    private float coolTimeToSpawn;
+
+    private float tempCoolTime;
+
+    private void Start()
+    {
+        numberToSpawn = 3;
+
+        coolTimeToSpawn = 3f;
+
+        tempCoolTime = 1.5f;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) //TODO: 임시 스폰
+        tempCoolTime -= Time.deltaTime;
+
+        if (tempCoolTime <= 0)
         {
-            Instantiate(enemy[0], transform.position + new Vector3(Random.Range(-20f, 20f), 0, 0), transform.rotation);
+            transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(-180f, 180f), 0));
+
+            if (enemy.Length > 0 && placeToSpawnEnemy != null)
+            {
+                for (int i = 0; i < numberToSpawn; i++)
+                {
+                    Instantiate(enemy[0], placeToSpawnEnemy.position + new Vector3(Random.Range(-20f, 20f), 0, 0), transform.rotation);
+                }
+            }
+
+            tempCoolTime = coolTimeToSpawn;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
         }
     }
 }
