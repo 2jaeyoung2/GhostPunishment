c8d0e0b807e691f459eaa27ccf8866ec1844b127
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-02-20 10:42:04 +0900

    [Fix]Keep following issue
    ghost following player while player is in range of ghost attack

--- Assets/Scripts/Units/Enemy/BasicGhost/BasicGhost.cs
+++ Assets/Scripts/Units/Enemy/BasicGhost/BasicGhost.cs
@@ -52,7 +52,7 @@ public class BasicGhost : EnemyManager
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
